d7c7f431cb37dd11521186574592bd0901ed307f
shpertakhia <shpertakhia@napr.gov.ge>
2022-10-05 17:06:12 +0400

    layer group switcher logic

--- main.js
+++ main.js
@@ -88,32 +88,36 @@ function init() {
     target: "js-map",
   });
 
-  // Layer Group
-  const layerGroup = new ol.layer.Group({
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM({
-          url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        }),
-        zIndex: 0,
-        visible: false,
-        extent: [
-          4218834.764360715, 4863270.25403115, 5418998.024475696,
-          5572932.007838269,
-        ],
-      }),
-      // Bing Maps Basemap Layer
-      new ol.layer.Tile({
-        source: new ol.source.BingMaps({
-          key: "AkIO7_Vz3poCELR3i1vLHUPe1pxT10SzGyG0ul6h3kH0SOHCeIZ6312JVb_PnA7-",
-          imagerySet: "AerialWithLabels", // Options 'Aerial', 'AerialWithLabels', 'Road'
-        }),
-        visible: true,
-        opacity: 0.7,
-      }),
-    ],
+  //Base Layers 
+  //Opentreet Map Standard
+  const opentreetMapStandard = new ol.layer.Tile({
+    source: new ol.source.OSM(),
+    visible: true,
+    title: "OSMStandard",
   });
-  map.addLayer(layerGroup);
+
+  //Opentreet Map Humanitarian
+  const opentreetMapHumanitarian = new ol.layer.Tile({
+    source: new ol.source.OSM({
+      url: "https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+    }),
+    extent: [
+      4218834.764360715, 4863270.25403115, 5418998.024475696,
+      5572932.007838269,
+    ],
+    visible: false,
+    title: "OSMHumanitarian",
+  });
+
+  // Bing Maps
+  const bingMaps = new ol.layer.Tile({
+    source: new ol.source.BingMaps({
+      key: "AkIO7_Vz3poCELR3i1vLHUPe1pxT10SzGyG0ul6h3kH0SOHCeIZ6312JVb_PnA7-",
+      imagerySet: "AerialWithLabels", // Options 'Aerial', 'AerialWithLabels', 'Road'
+    }),
+    visible: false,
+    title: "BingMaps",
+  });
 
   // CartoDB Basemap Layer
   const cartoDBBaseLayer = new ol.layer.Tile({
@@ -121,44 +125,93 @@ function init() {
       url: "https://{a-c}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
     }),
     visible: false,
+    title: "CartoDarkAll",
   });
-  map.addLayer(cartoDBBaseLayer);
+
 
   // TileDebug Layer
-    const tileDebugLayer = new ol.layer.Tile({
-        source: new ol.source.TileDebug({
-            projection: 'EPSG:3857',
-            tileGrid: new ol.tilegrid.TileGrid({
-                extent: [
-                    4218834.764360715, 4863270.25403115, 5418998.024475696,
-                    5572932.007838269,
-                ],
-                resolutions: [
-                    156543.03392804103, 78271.5169640205, 39135.75848201024,
-                    19567.87924100512, 9783.93962050256, 4891.96981025128,
-                    2445.98490512564, 1222.99245256282, 611.49622628141,
-                    305.748113140705, 152.8740565703525, 76.43702828517625,
-                    38.21851414258813, 19.109257071294063, 9.554628535647032,
-                    4.777314267823516, 2.388657133911758, 1.194328566955879,
-                    0.5971642834779395, 0.29858214173896974, 0.14929107086948487,
-                    0.07464553543474244, 0.03732276771737122,
-                ],
-                origin: [-20037508.342789244, 20037508.342789244],
-            }),
-        }),
-        visible: false,
-    });
-    map.addLayer(tileDebugLayer);
+  const tileDebugLayer = new ol.layer.Tile({
+    source: new ol.source.TileDebug({
+      projection: "EPSG:3857",
+      tileGrid: new ol.tilegrid.TileGrid({
+        extent: [
+          4218834.764360715, 4863270.25403115, 5418998.024475696,
+          5572932.007838269,
+        ],
+        resolutions: [
+          156543.03392804103, 78271.5169640205, 39135.75848201024,
+          19567.87924100512, 9783.93962050256, 4891.96981025128,
+          2445.98490512564, 1222.99245256282, 611.49622628141, 305.748113140705,
+          152.8740565703525, 76.43702828517625, 38.21851414258813,
+          19.109257071294063, 9.554628535647032, 4.777314267823516,
+          2.388657133911758, 1.194328566955879, 0.5971642834779395,
+          0.29858214173896974, 0.14929107086948487, 0.07464553543474244,
+          0.03732276771737122,
+        ],
+        origin: [-20037508.342789244, 20037508.342789244],
+      }),
+    }),
+    visible: false,
+  });
+  map.addLayer(tileDebugLayer);
 
-    // Stamen basemap layer
-    const stamenBaseLayer = new ol.layer.Tile({
-        source: new ol.source.Stamen({
-            layer: 'watercolor',
-        }),
-        visible: false,
-    });
-    map.addLayer(stamenBaseLayer);
+  // Stamen basemap layer
+  const stamenBaseLayer = new ol.layer.Tile({
+    source: new ol.source.Stamen({
+      layer: "watercolor",
+    }),
+    visible: false,
+  });
+  map.addLayer(stamenBaseLayer);
 
+  // georgian basemap layer
+  const layerNorv = new ol.layer.Image({
+    source: new ol.source.ImageWMS({
+      url: "http://mp1.napr.gov.ge/ORTHO_2016-17_NORV/service?",
+      crossOrigin: "anonymous",
+      params: {
+        LAYERS: "ORTHO_2016-17_NORV",
+        FORMAT: "image/png",
+        SRS: "EPSG:900913",
+        CRS: "EPSG:3857",
+        EXCEPTIONS: "application/vnd.ogc.se_inimage",
+        SERVICE: "WMS",
+        REQUEST: "GetMap",
+        VERSION: "1.3.0",
+      },
+      serverType: "mapserver",
+    }),
+    visible: false,
+    title: "layerNorv",
+  });
+  map.addLayer(layerNorv);
+
+  // console.log(stamenBaseLayer.getSource().getKeys());
+
+
+  const baseLayersGroup = new ol.layer.Group({
+    layers: [
+      opentreetMapStandard,
+      opentreetMapHumanitarian,
+      bingMaps,
+      cartoDBBaseLayer,
+      stamenBaseLayer,
+      layerNorv,
+    ],
+  });
+  map.addLayer(baseLayersGroup);
+
+  // Layer Switcher logic for BaseLayers
+  const baseLayerElements = document.querySelectorAll(".sidebar > input[type=radio]");
+  for (let baseLayerElement of baseLayerElements) {
+    baseLayerElement.addEventListener("change", function () {
+      let baseLayerElementValue = this.value;
+      baseLayersGroup.getLayers().forEach(function (element, index, array) {
+        let baseLayerTitle = element.get("title");
+        element.setVisible(baseLayerTitle === baseLayerElementValue);
+      });
+    });
+  }
 
   map.on("click", function (event) {
     console.log(event);
